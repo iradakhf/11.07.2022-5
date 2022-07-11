4eae335a8ff185e41c537de4c1c393236e7057de
iradakhf <iradakhf@code.edu.az>
2022-07-11 13:11:53 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ConsoleApp33
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            EvenOdd evenOrOdd = new EvenOdd(4);
+           bool i= evenOrOdd.FindEvenOrOdd(evenOrOdd.Number);
+            Console.WriteLine($"{evenOrOdd.Number} {i}");
+        }
+    }
+    class EvenOdd
+    {
+        public int Number { get; set; }
+        public EvenOdd(int number)
+        {
+            this.Number = number;
+        }
+        public bool FindEvenOrOdd(int number)
+        {
+            bool isEven = false;
+            if (number % 2 == 0)
+            {
+                isEven = true;
+            }
+            else
+            {
+                isEven = false;
+            }
+            return isEven;
+        }
+    }
+}
